1ae322695777dfaf45d325110eb1158fec5fabf9
hunchmen <valalberto0214@gmail.com>
2023-04-14 16:32:56 +0800

    initial commit

---
+++ src/main/java/com/via/movies/MoviesApplication.java
@@ -0,0 +1,26 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2023. All Rights Reserved.
+ * 
+ */
+package com.via.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 14 Apr 2023
+ *
+ */
+@SpringBootApplication
+public class MoviesApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MoviesApplication.class, args);
+    }
+
+}
